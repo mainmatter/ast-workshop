c9760b98de541af1aad671fc27d009c92c4797f0
Tobias Bieniek <tobias.bieniek@gmail.com>
2020-01-26 16:29:35 +0100

    08: Add example code

---
+++ 08-no-console-log/.eslintrc.js
@@ -0,0 +1,10 @@
+module.exports = {
+  root: true,
+  parserOptions: {
+    ecmaVersion: 2018,
+    sourceType: 'module',
+  },
+  rules: {
+    'no-console': 'error',
+  },
+};
---
+++ 08-no-console-log/app/adapters/api-token.js
@@ -0,0 +1,18 @@
+import RESTAdapter from '@ember-data/adapter/rest';
+
+export default RESTAdapter.extend({
+  namespace: 'api/v1/me',
+  pathForType() {
+    return 'tokens';
+  },
+  createRecord(store, type, snapshot) {
+    let data = {};
+    let serializer = store.serializerFor(type.modelName);
+    let url = this.buildURL(type.modelName, null, snapshot, 'createRecord');
+
+    console.log(type);
+    serializer.serializeIntoHash(data, type, snapshot, { includeId: true });
+
+    return this.ajax(url, 'PUT', { data });
+  },
+});
